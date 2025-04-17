bea1508bc711cb2e80cde756e0ac33f4bb65af61
mhd kdaimati <36774767+mhdkdaimati@users.noreply.github.com>
2025-04-17 17:35:40 +0200

    dev

--- assystor-api/database/migrations/2025_04_17_151244_create_customers_table.php
+++ assystor-api/database/migrations/2025_04_17_151244_create_customers_table.php
@@ -11,22 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['admin', 'manager', 'operator']);
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
-
-    }
+        }
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('customers');
     }
 };
--- assystor-api/database/migrations/2025_04_17_152906_create_customers_table.php
+++ assystor-api/database/migrations/2025_04_17_152906_create_customers_table.php
@@ -11,15 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['admin', 'manager', 'operator']);
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
-
+    
     }
 
     /**
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('customers');
     }
 };
