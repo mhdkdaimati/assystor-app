a114de20dba1910c35690c095264b61b408ab614
mhd kdaimati <36774767+mhdkdaimati@users.noreply.github.com>
2025-04-17 15:52:05 +0200

    dev

--- assystor-ui/src/App.jsx
+++ assystor-ui/src/App.jsx
@@ -4,20 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import CustomerList from './components/CustomerList';
 import CustomerForm from './components/CustomerForm';
+import Login from './components/Login';
 
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div className="App">
-        <h1>Customer App</h1>
-        <CustomerList />
-        <CustomerForm />
-      </div>
-    </>
-  )
-}
+  return <Login />;}
 
 export default App
--- assystor-ui/src/main.jsx
+++ assystor-ui/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext';
+import AdminRoute from './routes/AdminRoute';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';  // تأكد من أن المسار صحيح
+import Login from './components/Login';
+import AdminDashboard from './components/AdminDashboard';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
+      </Routes>
+    </BrowserRouter>
+  </AuthProvider>
+);
