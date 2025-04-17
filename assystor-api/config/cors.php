3961d5fe9ebf9f27f518a95e006c195319a178ce
mhd kdaimati <36774767+mhdkdaimati@users.noreply.github.com>
2025-04-17 17:11:26 +0200

    dev

--- assystor-api/config/cors.php
+++ assystor-api/config/cors.php
@@ -15,12 +15,12 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-'allowed_origins' => [
-    env('FRONTEND_URL', 'http://localhost:5173')],
+    'allowed_origins' => ['*'],
+
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
--- assystor-ui/src/components/AdminDashboard.jsx
+++
@@ -1,8 +0,0 @@
-export default function AdminDashboard() {
-    return (
-      <div>
-        <h2>Admin Dashboard</h2>
-      </div>
-    );
-  }
-  
