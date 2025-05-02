0efa776c048d8b953c924e3fed08b2afdaae30e3
mhd kdaimati <36774767+mhdkdaimati@users.noreply.github.com>
2025-05-02 10:32:31 +0200

    رفع جميع الملفات إلى GitLab

--- assystor-ui/src/components/customer/AddCustomer.jsx
+++ assystor-ui/src/components/customer/AddCustomer.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-// import swal from 'sweetalert';
+import swal from 'sweetalert';
 import { useNavigate, Link } from 'react-router-dom';
 
 
