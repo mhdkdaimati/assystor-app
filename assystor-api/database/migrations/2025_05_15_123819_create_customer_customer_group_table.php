2a1e75b6b73cfdb4b99dcf82e3f5c811e62b101f
mohamed khereldin kdaimaty <m.kdaimaty@yahoo.com>
2025-05-15 15:54:25 +0200

    fix bugs

--- assystor-api/database/migrations/2025_05_15_123819_create_customer_customer_group_table.php
+++ assystor-api/database/migrations/2025_05_15_123819_create_customer_customer_group_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('customer_customer_group', function (Blueprint $table) {
             $table->foreignId('customer_id')->constrained()->onDelete('cascade');
             $table->foreignId('customer_group_id')->constrained()->onDelete('cascade');
-            $table->string('status')->nullable();
+            $table->string('status')->default('incomplete');
             $table->foreignId('employee_id')->nullable()->constrained('users')->onDelete('set null');
             $table->primary(['customer_id', 'customer_group_id']); // 
         });
