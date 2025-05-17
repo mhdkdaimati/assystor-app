55c613791ef0c31e518803e68f6f38cee761fbaa
mohamed khereldin kdaimaty <m.kdaimaty@yahoo.com>
2025-05-17 22:52:57 +0200

    fix bugs

--- assystor-api/vite.config.js
+++ assystor-api/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true
+  }
+})
