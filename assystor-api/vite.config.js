c15fc72066246fc6029751deaccd006146b5927c
mohamed khereldin kdaimaty <m.kdaimaty@yahoo.com>
2025-05-17 23:06:41 +0200

    config

--- assystor-api/vite.config.js
+++ assystor-api/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true
-  }
-})
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
