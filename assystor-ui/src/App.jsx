94ceafe235376112186a90c314a7216ac715eb42
mohamed khereldin kdaimaty <m.kdaimaty@yahoo.com>
2025-05-18 19:18:15 +0200

    url

--- assystor-ui/src/App.jsx
+++ assystor-ui/src/App.jsx
@@ -36,7 +36,7 @@ import { useState, useEffect } from 'react';
 
 import CreateProduct from './components/product/CreateProduct';
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "http://192.168.25.245:8000/";
+axios.defaults.baseURL = "http://192.168.176.245:8000/";
 // axios.defaults.baseURL = "http://127.0.0.1:8000/";
 // 
 axios.defaults.headers.post['Accept'] = 'application/json';
